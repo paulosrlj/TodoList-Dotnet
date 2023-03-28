2df4129e1df98827fef474a88042db0c4460d063
Paulo <paulo.leite@academico.ifpb.edu.br>
2023-03-27 22:15:41 -0300

    Add: User repository

--- TodoList/Repositories/Concrete/UserRepository.cs
+++ TodoList/Repositories/Concrete/UserRepository.cs
@@ -1,9 +1,39 @@
-﻿using TodoList.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Data;
+using TodoList.Models.Domain;
+using TodoList.Repositories.Interfaces;
 
 namespace TodoList.Repositories.Concrete
 {
     public class UserRepository : IUserRepository
     {
 
+        private readonly TodoListDbContext _ctx;
+        private readonly ILogger<IUserRepository> _logger;
+
+        public UserRepository(TodoListDbContext ctx, ILogger<IUserRepository> logger)
+        {
+            _ctx = ctx;
+            _logger = logger;
+        }
+
+        public async Task<User> Create(User user)
+        {
+            await _ctx.AddAsync(user);
+            await _ctx.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> FindByEmail(string email)
+        {
+            var user = await _ctx.Users.FirstAsync(u => u.Email == email);
+            return user;
+        }
+
+        public async Task<User> FindById(Guid id)
+        {
+            var user = await _ctx.Users.FirstAsync(u => u.Id == id);
+            return user;
+        }
     }
 }
--- TodoList/Repositories/Interfaces/IUserRepository.cs
+++ TodoList/Repositories/Interfaces/IUserRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace TodoList.Repositories.Interfaces
+﻿using TodoList.Models.Domain;
+
+namespace TodoList.Repositories.Interfaces
 {
     public interface IUserRepository
     {
-
+        Task<User> Create(User user);
+        Task<User> FindById(Guid id);
+        Task<User> FindByEmail(string email);
     }
 }
